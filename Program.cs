fdbf9af655dad70c7eb4257b7724289ecd4c384c
giri <girim7593@gmail.com>
2021-11-13 09:09:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_2019__prDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
